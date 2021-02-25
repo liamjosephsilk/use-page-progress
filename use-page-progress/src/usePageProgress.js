cda1ec55362a45b177a31599ab50f343ac53641c
Liam Silk <liamjosephsilk@gmail.com>
2021-02-25 10:45:09 +0000

    Refactor for isolating functions

--- use-page-progress/src/usePageProgress.js
+++ use-page-progress/src/usePageProgress.js
@@ -1,37 +1,38 @@
 import { useEffect, useState } from "react";
 
+const calculateScrollDistance = () => {
+  if (typeof window !== "undefined") {
+    const scrollTop = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+    const documentHeight = getDocumentHeight();
+
+    const totalDocumentScrollHeight = documentHeight - windowHeight;
+    const scrollPosition = Math.floor(
+      (scrollTop / totalDocumentScrollHeight) * 100
+    );
+
+    return scrollPosition;
+  }
+};
+
+const getDocumentHeight = () => {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+};
+
 const useScrollProgress = () => {
   const [scrollPercentage, setScrollPercentage] = useState(0);
 
-  const calculateScrollDistance = () => {
-    if (typeof window !== "undefined") {
-      const scrollTop = window.pageYOffset;
-      const windowHeight = window.innerHeight;
-      const documentHeight = getDocumentHeight();
-
-      const totalDocumentScrollHeight = documentHeight - windowHeight;
-      const scrollPosition = Math.floor(
-        (scrollTop / totalDocumentScrollHeight) * 100
-      );
-
-      setScrollPercentage(scrollPosition);
-      return;
-    }
-  };
-
-  const getDocumentHeight = () => {
-    return Math.max(
-      document.body.scrollHeight,
-      document.documentElement.scrollHeight,
-      document.body.offsetHeight,
-      document.documentElement.offsetHeight,
-      document.body.clientHeight,
-      document.documentElement.clientHeight
-    );
-  };
-
   useEffect(() => {
-    window.addEventListener("scroll", () => calculateScrollDistance());
+    window.addEventListener("scroll", () =>
+      setScrollPercentage(calculateScrollDistance())
+    );
   }, []);
 
   return { scrollPercentage };
